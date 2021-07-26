0fc1687801a0815309675fdc2e77e7ca64c19f25
Florian MESA--GOUIFFES <mesagouiffes@gmail.com>
2021-07-26 15:45:57 +0200

    Socket hats

---
+++ My Virtual Room/Assets/_Course Library/Scripts/InifiiteRotation.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InifiiteRotation : MonoBehaviour
+{
+    public Vector3 rotationDirection;
+    public float smoothTime;
+    private float convertedTime = 200;
+    private float smooth;
+
+    // Update is called once per frame
+    void Update()
+    {
+        smooth = Time.deltaTime * smoothTime * convertedTime;
+        transform.Rotate(rotationDirection * smooth);
+    }
+}
